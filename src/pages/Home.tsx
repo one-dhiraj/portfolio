d9913b0cc60434f29621e8cacb3d06f3e9193bc4
Dhiraj Kumar Sharma <dhirajksharma.social@gmail.com>
2025-02-04 12:54:50 +0530

    updated social links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,8 @@
 import { Box, Flex, Icon, Image, Text } from '@chakra-ui/react'
-import me from "../assets/me.jpg"
+import me from "../assets/me.png"
 import { Link } from 'react-router-dom'
-import { FaLinkedinIn } from "react-icons/fa6";
-import { GrGithub } from "react-icons/gr";
-import { IoMdMail } from "react-icons/io";
-
+import { FaEnvelope, FaGithub, FaGooglePlay, FaLinkedinIn } from "react-icons/fa6";
+import { SiLeetcode } from 'react-icons/si';
 
 export default function Home() {
   return (
@@ -14,17 +12,58 @@ export default function Home() {
       gap={{base: 0, md: 8}}
       align={"center"}
       justifyContent={"center"}
-    >
+      >
       {/* Left Side - Image */}
-      <Box flexShrink={0}>
+      <Flex
+        boxSize={{base: '100vw', md: '500px'}}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Flex
+        boxSize={{base: '92vw', md: '475px'}}
+        borderWidth={1}
+        borderColor="black"
+        borderRadius={"full"}
+        position="absolute"
+        align={"center"}
+      justifyContent={"center"}
+        >
+        <Flex
+        boxSize={{base: '250px', md: '350px'}}
+        borderWidth={1}
+        borderColor="black"
+        borderRadius={"full"}
+        position="absolute"
+        align={"center"}
+      justifyContent={"center"}
+        >
         <Image
           src={me}
           alt="Dhiraj's Image"
           borderRadius="full"
-          boxSize={{ base: '250px', md: '450px' }}
+          boxSize={{ base: '250px', md: '350px' }}
           objectFit="cover"
+          position="absolute"
+          top="5%"
         />
-      </Box>
+          <Link to="https://leetcode.com/dhirajksharma" target='_blank'>
+            <Icon position={"absolute"} padding={1} top="11%" left={{base: "5%", md: "7%"}} as={SiLeetcode} boxSize={{base: 9, md: 10}} bg="white" />
+          </Link>
+          <Link to="https://play.google.com/store/apps/dev?id=5273581280074097918" target='_blank'>
+            <Icon position={"absolute"} padding={1} bottom="25%" right={{base: "-4%", md: "-2%"}} as={FaGooglePlay} boxSize={{base: 9, md: 10}} bg="white" />
+          </Link>
+        </Flex>
+          <Link to="mailto:dhirajksharma.official@gmail.com" target='_blank'>
+            <Icon position={"absolute"} padding={1} top={{base: "10%", md: "11%"}} right="10%" as={FaEnvelope} boxSize={{base: 9, md: 10}} bg="white" />
+          </Link>
+          <Link to="https://linkedin.com/in/dhirajksharma29" target='_blank'>
+            <Icon position={"absolute"} padding={1} top="50%" left="-3%" as={FaLinkedinIn} boxSize={{base: 9, md: 10}} bg="white" />
+          </Link>
+          <Link to="https://github.com/one-dhiraj" target='_blank'>
+            <Icon position={"absolute"} padding={1} bottom="-1%" right="30%" as={FaGithub} boxSize={{base: 9, md: 10}} bg="white" />
+          </Link>
+        </Flex>
+      </Flex>
 
       {/* Right Side - Text */}
       <Box textAlign={{ base: 'center', md: 'left' }} mt={4}>
@@ -35,17 +74,6 @@ export default function Home() {
         <Text fontSize={{ base: 'lg', md: 'xl' }} fontFamily={'Montserrat'} px={3}>
           I think and I make things<br></br>websites, apps, and stories.
         </Text>
-        <Flex mt={4} gap={3} px={3} justifyContent={{base: "center", md: "flex-start"}} alignItems={"center"}>
-          <Link to="mailto:dhirajksharma.official@gmail.com" target='_blank'>
-            <Icon as={IoMdMail} boxSize={9} />
-          </Link>
-          <Link to="https://linkedin.com/in/dhirajksharma29" target='_blank'>
-            <Icon as={FaLinkedinIn} boxSize={8} />
-          </Link>
-          <Link to="https://github.com/dhirajksharma" target='_blank'>
-            <Icon as={GrGithub} boxSize={8} mb={0.5} />
-          </Link>
-        </Flex>
       </Box>
     </Flex>
   )
